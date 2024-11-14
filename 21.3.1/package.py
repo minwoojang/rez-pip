3da99b75dd69c621ffab4945e63d66a7646e7db8
minwoojang <q860514@naver.com>
2024-11-14 11:51:46 +0900

    21.3.1 build

---
+++ 21.3.1/package.py
@@ -0,0 +1,32 @@
+name = "pip"
+
+version = "21.3.1"
+
+authors = [
+    "Chris Jerdonek",
+    "Donald Stufft"
+]
+
+description = \
+    """
+    The PyPA recommended tool for installing Python packages.
+    """
+
+requires = [
+    "cmake-3+",
+    "python-3-7"
+]
+
+variants = [
+    ["platform-linux", "arch-x86_64"]
+]
+
+build_system = "cmake"
+
+with scope("config") as config:
+    config.build_thread_count = "logical_cores"
+
+uuid = "pip-{version}".format(version=str(version))
+
+def commands():
+    env.PYTHONPATH.append("{root}")
